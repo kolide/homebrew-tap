d288ac91cffa8d91579f13572244b3370f5c75f8
seph <seph@directionless.org>
2018-11-12 21:02:50 -0500

    Update to 0.1.0-138

--- Formula/kolide.rb
+++ Formula/kolide.rb
@@ -2,9 +2,9 @@ class Kolide < Formula
   desc "Command-Line Client for Kolide"
   homepage "https://kolide.com/"
 
-  url "https://storage.googleapis.com/kolide-downloads/kolide/0.1.0-28-g75aff00/darwin/kolide.zip"
-  version "0.1.0-28+g75aff00"
-  sha256 "70b784d844d25fbac814fa1b7f41deda38d541daf4a35e43646a44dbd9182126"
+  url "https://storage.googleapis.com/kolide-downloads/kolide/0.1.0-138-g718a0d6/darwin/kolide.zip"
+  version "0.1.0-138+g718a0d6"
+  sha256 "a23b0626006dbd4b066e625d919b314965442be569a296c3a07b4f6828dfbfa6"
 
   head "https://storage.googleapis.com/kolide-downloads/kolide/latest/darwin/kolide.zip"
 
